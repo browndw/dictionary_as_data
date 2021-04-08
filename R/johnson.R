d992defab5a985fe9a32530c838da05a0a104630
browndw <davidwestbrown@gmail.com>
2021-04-08 17:22:53 -0400

    Update johnson.R

--- R/johnson.R
+++ R/johnson.R
@@ -7,57 +7,62 @@ source("R/utils.R")
 johnson_files <- list.files("data/dictionary_data/johnson", pattern = "*.html", full.names = T)
 
 # Extract the authors with quotations in the dictionary
-cited_johnson <- lapply(johnson_files, extract_names)
+cited_johnson <- johnson_cited(johnson_files[1])
 
-# Comine into a single dataframe
-cited_johnson <- bind_rows(cited_johnson)
-
-# Read in a table containing all of the books in the Bible
-# in order to count not as separate books but as Biblical references
-bible_books <- read_csv("data/data_tables/bible_list.csv")
-
-# Reformat Biblical references
-cited_johnson <- cited_johnson %>% 
-  mutate(author = ifelse(!is.na(bible_books$source[match(cited_johnson$author, bible_books$book)]),
-                          bible_books$source[match(cited_johnson$author, bible_books$book)], author))   
-
-# Tally cited authors
-cited_johnson <- cited_johnson %>%
-  count(author) %>%
-  arrange(-n) %>%
+# Clean the data a little
+cited_johnson <- clean_entries_johnson(cited_johnson) %>%
   rownames_to_column("rank_johnson") %>%
   rename(n_johnson = n)
 
-# Get the paths to the files containing Webster's dictionary
-websters_files <- list.files("data/dictionary_data/websters_1913/xml_files", pattern = "_\\w.xml", full.names = T)
+# Compare to the counts reported by Willinsky
+willinsky <- read_csv("data/data_tables/willinsky_counts.csv") %>%
+  left_join(cited_johnson) %>%
+  mutate(error = (n_johnson - n_willinsky)/n_willinsky*100) %>%
+  filter(author != "Wallace")
+
+# Calculate the mean error (assuming Willisky's counts are accurate)
+mean(willinsky$error)
+
+# Get the paths to the files containing Webster's Unabridged dictionary
+websters_files <- list.files("data/dictionary_data/websters/websters_1913/xml_files", pattern = "_\\w.xml", full.names = T)
 
 # Extract the authors with quotations in the dictionary
-cited_websters <- lapply(websters_files, extract_authors)
+cited_websters_1913 <- lapply(websters_files, websters_cited)
+
+cited_websters_1913 <- bind_rows(cited_websters_1913) %>%
+  group_by(author) %>%
+  summarize(n_websters_2 = sum(n)) %>%
+  arrange(-n_websters_2) %>%
+  rownames_to_column("rank_websters_2")
 
 # Format and tally citations
-cited_websters <- unlist(cited_websters) %>% unname() %>% table() %>% as_tibble() %>% 
-  rename_with(~ gsub(".", "author", .x, fixed = TRUE)) %>% 
-  mutate(author = str_remove(author, ".$")) %>%
-  mutate(author = str_remove(author, "^[[:punct:]]")) %>%
-  mutate(author = str_squish(author)) %>%
-  mutate(author = ifelse(str_detect(author, "\\. \\d+"), "bible", author)) %>%
-  mutate(author = ifelse(str_detect(author, "^\\d"), "bible", author)) %>%
-  mutate(author = ifelse(str_detect(author, "^shak"), "shakespeare", author)) %>%
-  mutate(author = textclean::replace_html(author)) %>%
+websters_1841 <- read_csv("data/dictionary_data/websters/websters_1841.csv")
+
+cited_websters_1841 <- str_extract(websters_1841$definition, "– [A-Z].*?\\.$") %>%
+  str_split(" – ") %>%
+  unlist() %>%
+  str_split("(?<=[a-z-][a-z-]\\.) (?=[A-Z][a-z-])") %>%
+  unlist() %>%
+  data.frame(author = .) %>%
+  filter(!is.na(author)) %>%
+  mutate(author = str_remove(author, "– ")) %>%
+  mutate(author = ifelse(str_detect(author, " [i|v|x|l|c|d|m]+(\\.|,)"), "Bible", author)) %>%
+  mutate(author = str_remove(author, "'s .*?$")) %>%
+  mutate(author = str_remove(author, "\\.$")) %>%
+  mutate(author = ifelse(str_detect(author, "Shak"), "Shakespeare", author)) %>%
+  mutate(author = ifelse(str_detect(author, "^Brown$"), "Browne", author)) %>%
+  filter(!str_detect(author, "^Dict|^Cyc|Encyc")) %>%
+  filter(str_count(author, " ") < 4) %>%
   group_by(author) %>%
-  summarize(n = sum(n)) %>%
-  mutate(author = str_to_title(author)) %>%
-  mutate(author = ifelse(author == "L'estrange", "L'Estrange", author)) %>%
-  mutate(author = ifelse(author == "Sir T. Browne", "Browne", author)) %>%
-  mutate(author = ifelse(author == "Sir P. Sidney", "Sidney", author)) %>%
+  tally() %>%
   arrange(-n) %>%
-  rownames_to_column("rank_websters") %>%
-  rename(n_websters = n)
+  rename(n_websters_1 = n) %>%
+  rownames_to_column("rank_websters_1")
 
 # Combine counts into a single dataframe
-cited_all <- full_join(cited_johnson, cited_websters, by = "author") %>%
-  select(author, n_johnson, n_websters, rank_johnson, rank_websters) %>%
+cited_all <- full_join(cited_johnson, cited_websters_1841, by = "author") %>%
+  select(author, n_websters_1, n_johnson, rank_websters_1, rank_johnson) %>%
   mutate(rank_johnson = as.numeric(rank_johnson)) %>%
-  mutate(rank_websters = as.numeric(rank_websters))
+  mutate(rank_websters_1 = as.numeric(rank_websters_1))
 
 
